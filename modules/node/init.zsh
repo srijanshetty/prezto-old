6c2a3cc3fab2cf1a55b0b390afdb34bc32151d67
Srijan R Shetty <srijan.shetty@gmail.com>
2016-07-30 16:55:13 +0530

    Update node module to allow faster loadup of nvm

--- modules/node/init.zsh
+++ modules/node/init.zsh
@@ -8,7 +8,19 @@
 
 # Load manually installed NVM into the shell session.
 if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-  source "$HOME/.nvm/nvm.sh"
+  declare -a NODE_GLOBALS=(`find ~/.nvm/versions/node -maxdepth 3 -type l -wholename '*/bin/*' | xargs -n1 basename | sort | uniq`)
+
+  NODE_GLOBALS+=("node")
+  NODE_GLOBALS+=("nvm")
+
+  load_nvm () {
+      export NVM_DIR=~/.nvm
+      [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+  }
+
+  for cmd in "${NODE_GLOBALS[@]}"; do
+      eval "${cmd}(){ unset -f ${NODE_GLOBALS}; load_nvm; ${cmd} \$@ }"
+  done
 
 # Load package manager installed NVM into the shell session.
 elif (( $+commands[brew] )) && [[ -d "$(brew --prefix nvm 2>/dev/null)" ]]; then
